1b1e725db24dc5ab17876cfb20fe1be37675321c
RachelKolk <rach.m.obrien@gmail.com>
2019-05-06 16:48:05 -0400

    Bubble sort coded and tested - still have to figure out time complexity

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,7 +20,23 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    # set swap_occured to default true so that the while loop fires
+    swap_occured = True
+    while swap_occured:
+        # 1. Loop through your array
+        # set swap to false, so that when swaps no longer happen the loop will break/end
+        swap_occured = False
+        for i in range(0, len(arr)-1):
+            # create temp variable to hold item to swap
+            temp = arr[i]
+            # Compare each element to its neighbor
+            if arr[i] > arr[i + 1]:
+                # If elements in wrong position (relative to each other, swap them)
+                arr[i] = arr[i+1]
+                # uses the temp value to populate the i+1 index if a swapped occured
+                arr[i + 1] = temp
+                swap_occured = True
+            i +=1
     return arr
 
 
