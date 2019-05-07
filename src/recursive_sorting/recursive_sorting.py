7a64b1ffa5a0e96885cadae3fc03cad4f17eeec1
RachelKolk <rach.m.obrien@gmail.com>
2019-05-07 17:04:05 -0400

    Working on the merge sort algorithm solution

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,43 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    merged_arr = []
     # TO-DO
-    
+    print("Merging ", arrA, " and ", arrB)
+    for i in arrA:
+        for x in arrB:
+            if i > x:
+                merged_arr.append(x)
+                merged_arr.append(i)
+            else:
+                merged_arr.append(i)
+                merged_arr.append(x)                
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-
-    return arr
+    if len(arr) > 1:
+        # 1. While your data set contains more than one item, split it in half
+        #divide arr into half recursively
+        print("Splitting array - length is " + str(len(arr)))
+        print("Slice", arr[0:int(len(arr)//2)])
+        left = merge_sort(arr[0:int(len(arr)//2)])
+        print("Slice", arr[int(len(arr)//2):])
+        right = merge_sort(arr[int(len(arr)//2):])
+        print("Left:", left)
+        print("Right:", right)
+       
+    # conquer using merge helper function
+    if len(arr) == 1:
+        return arr
+    if right != None:
+        if len(left) == len(right):
+            arr = merge(left, right)
+            return arr
+    else:
+        return left
 
 
 # STRETCH: implement an in-place merge sort algorithm
